7dd3e85a8b2ba686ff79ec0b71ae488e8cf1f1c4
Jacarte <javierca@kth.se>
2022-07-27 11:36:12 +0200

    WIP

--- RQ1/download_all.sh
+++ RQ1/download_all.sh
@@ -3,7 +3,10 @@ if [ -f filtered-binaries-metadata.7z ]
 then
     echo "Exist"
 else
+    mkdir -p filtered
+    cd filtered
     wget https://github.com/sola-st/WasmBench/releases/download/v1.0/filtered-binaries-metadata.7z
+    cd ..
 fi
 
 
@@ -11,12 +14,19 @@ if [ -f all-binaries-metadata.7z ]
 then
     echo "Exist"
 else
+    mkdir -p all
+    cd all
     wget https://github.com/sola-st/WasmBench/releases/download/v1.0/all-binaries-metadata.7z
+    cd ..
 fi
 
 
 
 
-
+cd all
 7z x all-binaries-metadata.7z
+cd ..
+
+cd filtered
 7z x filtered-binaries-metadata.7z
+cd ..
