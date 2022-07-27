bda00f855e329e1e942bba0bc4eea7051e419fab
Jacarte <javierca@kth.se>
2022-07-27 16:45:41 +0200

    Adding optional dbconn

---
+++ experiments/kube_setup/deploy.sh
@@ -0,0 +1,13 @@
+kubectl create ns argo
+kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/install.yaml
+kubectl create rolebinding default-admin --clusterrole=admin --serviceaccount=default:default
+
+#kubectl create ns minio
+#kubectl apply -n minio -f minio.yml 
+
+#kubectl patch cm -n argo workflow-controller-configmap --type merge --patch "$(cat config.yml)"
+
+#kubectl create secret generic argo-artifacts --from-literal=accesskey="minio" --from-literal=secretkey="minio123"
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
+
+#kubectl port-forward -n minio service/minio-service 9000
