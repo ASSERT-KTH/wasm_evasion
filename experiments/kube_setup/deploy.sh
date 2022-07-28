ddd1e40c5ad1cb87ba3e87f2e13064d860dfa20d
Jacarte <javierca@kth.se>
2022-07-28 09:58:34 +0200

    Adding experiments with k8s

--- experiments/kube_setup/deploy.sh
+++ experiments/kube_setup/deploy.sh
@@ -1,13 +1,29 @@
+kubectl delete cm workflow-controller-configmap -n argo
 kubectl create ns argo
-kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/install.yaml
+kubectl apply -n argo -f https://github.com/argoproj/argo-workflows/releases/download/v3.3.8/install.yaml
 kubectl create rolebinding default-admin --clusterrole=admin --serviceaccount=default:default
 
-#kubectl create ns minio
-#kubectl apply -n minio -f minio.yml 
+# kubectl patch configmap workflow-controller-configmap --patch '{"data":{"containerRuntimeExecutor":"pns"}}'
 
-#kubectl patch cm -n argo workflow-controller-configmap --type merge --patch "$(cat config.yml)"
+# restart argo
+kubectl scale deploy argo-server --replicas 0 -n argo 
+kubectl scale deploy workflow-controller --replicas 0 -n argo
 
-#kubectl create secret generic argo-artifacts --from-literal=accesskey="minio" --from-literal=secretkey="minio123"
-#kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
+kubectl scale deploy argo-server --replicas 1 -n argo
+kubectl scale deploy workflow-controller --replicas 1 -n argo 
 
-#kubectl port-forward -n minio service/minio-service 9000
+
+kubectl create ns minio
+kubectl apply -n minio -f minio.yml 
+
+kubectl patch cm -n argo workflow-controller-configmap --type merge --patch "$(cat config.yml)"
+kubectl describe configmap workflow-controller-configmap -n argo
+
+
+kubectl logs $(kubectl get po -n argo | grep 'argo-server' | awk '{print $1}') -n argo
+kubectl logs $(kubectl get po -n argo | grep 'workflow-controller' | awk '{print $1}') -n argo | grep pns
+
+kubectl create secret generic argo-artifacts --from-literal=accesskey="minio" --from-literal=secretkey="minio123"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
+
+kubectl port-forward -n minio service/minio-service 3434
