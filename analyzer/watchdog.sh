9365b473f8ca4d9cb9f07ebafd56ce32cd316cc6
Jacarte <javierca@kth.se>
2022-08-04 15:30:13 +0200

    WIP

--- analyzer/watchdog.sh
+++ analyzer/watchdog.sh
@@ -1,5 +1,5 @@
 file=$1
 
 
-mc alias set exp https://minio-service.minio:3434
+mc alias set exp http://minio-service.minio:3434
 mc --quiet cp "$1" exp/my-bucket/snapshots/ 
--- analyzer/watchdog_interval.sh
+++ analyzer/watchdog_interval.sh
@@ -4,7 +4,7 @@ interval=$2
 function save() {
 
 
-    mc alias set exp https://minio-service.minio:3434 
+    mc alias set exp http://minio-service.minio:3434 
 
     mc --quiet cp "$1" exp/my-bucket/snapshots/ 
 }
