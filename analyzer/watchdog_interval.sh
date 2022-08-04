0426c2cb11a9a604134da14b0068225044fa123a
Jacarte <javierca@kth.se>
2022-08-04 16:18:25 +0200

    W

--- analyzer/watchdog_interval.sh
+++ analyzer/watchdog_interval.sh
@@ -6,12 +6,12 @@ MINIO_ROOT_PASSWORD=minio123
 
 function save() {
 
-
-   mc config host add exp http://minio-service.minio:3434 minio minio123
-
-
     mc --quiet cp "$1" exp/my-bucket/snapshots/ 
 }
+
+
+mc config host add exp http://minio-service.minio:3434 minio minio123
+
 
 while true
 do
