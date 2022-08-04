46a03f72a032bc746413d33f13b71f64e0eb51d9
Jacarte <javierca@kth.se>
2022-08-04 15:42:21 +0200

    WIP

--- analyzer/watchdog.sh
+++ analyzer/watchdog.sh
@@ -1,5 +1,8 @@
 file=$1
 
+MINIO_ROOT_USER=minio
+MINIO_ROOT_PASSWORD=minio123
 
-mc alias set exp http://minio-service.minio:3434
+
+mc config host add exp http://minio-service.minio:3434 minio minio123
 mc --quiet cp "$1" exp/my-bucket/snapshots/ 
