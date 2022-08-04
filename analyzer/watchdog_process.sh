9b426653a61056cc42280c419e2820305156b596
Jacarte <javierca@kth.se>
2022-08-04 23:06:21 +0200

    WIP

--- analyzer/watchdog_process.sh
+++ analyzer/watchdog_process.sh
@@ -1,5 +1,5 @@
 sleep=$1
-th=$2
+th=$2 # in Kb
 shift 2
 command="$@"
 
@@ -16,15 +16,17 @@ do
     # sleep for a while and then check the mem
     sleep $sleep
     echo "Checking mem"
-    ps -o pid,%mem,command -a | grep "analyzer"
-    val=$(ps -o pid,%mem,command -a | grep analyzer | grep -v grep | awk {'print$2'})
-    underlyingid=$(ps -o pid,%mem,command -a | grep analyzer | grep -v grep | awk {'print$1'})
+    ps -o pid,rss,vsz,command -a | grep analyzer | grep -v grep | awk 'NR>1{$2=int($2/1024);$3=int($3/1024);}{print ;}'
+    val=$(ps -o pid,rss,vsz,command -a | grep analyzer | grep -v grep | awk 'NR>1{$2=int($2/1024);$3=int($3/1024);}{print ;}' | awk {'print$2'})
+    underlyingid=$(ps -o pid,command -a | grep analyzer | grep -v grep | awk {'print$1'})
+    echo "Analyzer process id $underlyingid"
+
     if [ -z "$val" ]; then
         echo "It seems the process is over"
         break
     fi
     
-    echo "Mem usage " $val
+    echo "Mem usage " $val "Mb"
     
 
     if (( $(echo "$val > $th" |bc -l) ))
