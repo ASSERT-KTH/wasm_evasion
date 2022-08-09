183d875aac722cfd8bf9c65cf678f17632db4eb2
Jacarte <javierca@kth.se>
2022-08-09 17:35:41 +0200

    WIP

--- analyzer/watchdog_interval.sh
+++ analyzer/watchdog_interval.sh
@@ -7,12 +7,12 @@ MINIO_ROOT_PASSWORD=minio123
 
 function save() {
 
-    mc --quiet cp "$1" exp/my-bucket/snapshots/$2 
+    mc --quiet cp "$1" exp/my-bucket/snapshots/$2/ 
 }
 
 function save_mem() {
     ps -o pid,user,%mem,command ax > $1.mem.log
-    mc --quiet cp $1.mem.log exp/my-bucket/snapshots/$2 
+    mc --quiet cp $1.mem.log exp/my-bucket/snapshots/$2/ 
 }
 
 
