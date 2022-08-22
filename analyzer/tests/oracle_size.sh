c7e89f3e10400890747371d21a6bb4be27e517c4
Jacarte <javierca@kth.se>
2022-08-22 15:54:30 +0200

    Bulk processing

--- analyzer/tests/oracle_size.sh
+++ analyzer/tests/oracle_size.sh
@@ -1,8 +1,12 @@
-file=$1
+files=$@
 
-if [ $(wc -c $file | awk '{printf $1}') -gt 40000 ]
-then
-    exit 1
-else
-    exit 0
-fi
+for file in $files
+do
+    echo $(wc -c $file | awk '{printf $1}')
+    if [ $(wc -c $file | awk '{printf $1}') -gt 40000 ]
+    then
+        exit 1
+    fi
+done
+
+exit 0
