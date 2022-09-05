6ea265668fd88a26cdb1b51622b8641d8bebf60f
Jacarte <javierca@kth.se>
2022-09-05 08:49:05 +0200

    Updating oracle

--- oracles/vt_custom_chrome/testcurl.sh
+++ oracles/vt_custom_chrome/testcurl.sh
@@ -1,2 +1,2 @@
-curl -X POST --user 'admin:admin' -F "file=@test.wasm" http://localhost:5000/upload_file
-curl --user 'admin:admin' http://localhost:5000/get_all_results
+curl -X POST --user 'admin:admin' -F "file=@test3.wasm" http://0.0.0.0:4000/upload_file/test2
+curl --user 'admin:admin' http://localhost:4000/get_all_results/test2
