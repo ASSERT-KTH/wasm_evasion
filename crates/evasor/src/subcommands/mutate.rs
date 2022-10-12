5b53a92ef7023268872185ce59be8cc340fce9c1
Jacarte <javierca@kth.se>
2022-10-12 09:48:30 +0200

    Penalize attempt

--- crates/evasor/src/subcommands/mutate.rs
+++ crates/evasor/src/subcommands/mutate.rs
@@ -21,7 +21,7 @@ use wasm_mutate::{
 use crate::{
     errors::{AResult, CliError},
     send_signal_to_probes_socket,
-    subcommands::mutate::{mutation_factory::get_by_name, hasting::{get_acceptance_symmetric_prob, get_distance_reward, get_distance_reward_and_size, get_distance_reward_penalize_iteration, get_distance_reward_penalize_attempt}},
+    subcommands::mutate::{mutation_factory::get_by_name, hasting::{get_acceptance_symmetric_prob, get_distance_reward, get_distance_reward_penalize_attempt, get_distance_reward_and_size, get_distance_reward_penalize_iteration, get_distance_reward_penalize_attempt}},
     State, SOCKET_PATH,
 };
 use std::thread;
@@ -750,7 +750,7 @@ pub fn mutate_with_reward(
                                 newr, all
                             ),
                             probs1clone,
-                            Box::new(get_distance_reward)
+                            Box::new(get_distance_reward_penalize_attempt)
                         );
 
                         let lg = rn.log(2.7) / beta;
