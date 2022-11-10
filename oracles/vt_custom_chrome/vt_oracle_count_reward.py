bfa4fe73aa7255f953a29e89c07ab891d1112614
Jacarte <javierca@kth.se>
2022-11-10 16:02:13 +0100

    R

--- oracles/vt_custom_chrome/vt_oracle_count_reward.py
+++ oracles/vt_custom_chrome/vt_oracle_count_reward.py
@@ -25,7 +25,7 @@ def check_simple(oracleurl, checkoracle, user, pass_, session, input):
 
     # submit the file
     # submit 2 times?
-    for _ in range(4):
+    for _ in range(2):
         r = requests.post(
             f"{oracleurl}/upload_file/{session}",
             files = { 'file': open(input, 'rb') },
