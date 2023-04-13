dabd59f8962392bad51a66e3013c1c63b53c4169
Javier Cabrera <javierca@kth.se>
2023-04-13 10:22:02 +0200

    Update vt_oracle_count.py

--- oracles/vt_custom_chrome/vt_oracle_count.py
+++ oracles/vt_custom_chrome/vt_oracle_count.py
@@ -1,6 +1,6 @@
 '''
     Takes a Wasm binary, and pass it to the oracle url as a file. Check the result and if its undetected or else in the specified antivirus, return 1
-    '''
+'''
 import sys
 import os
 import requests
@@ -14,59 +14,72 @@ WHITELIST = ['undetected', 'timeout', 'unable_to_process_file_type', 'no_respons
 
 def check_simple(oracleurl, checkoracle, user, pass_, session, input):
     global WHITELIST
-    
+
     # check count
     r = requests.get(
-                     f"{oracleurl}",
-                     auth = HTTPBasicAuth(user, pass_)
-                     )
-        
-                     print(r.text)
-                     
-                     # submit the file
-                     r = requests.post(
-                                       f"{oracleurl}/upload_file/{session}",
-                                       files = { 'file': open(input, 'rb') },
-                                       auth = HTTPBasicAuth(user, pass_)
-                                       )
-                     
-                     hsh = r.text
-                     print(hsh)
-                     
-                     lapsed = 0
-                     waitfor = 5
-                     while lapsed <= 900: # no more than 15 mins
-                         
-                         r = requests.get(
-                                          f"{oracleurl}/get_result/{session}/{hsh}",
-                                          auth = HTTPBasicAuth(user, pass_)
-                                          )
-                             if r.text == "REQUEUE":
-                                 requests.post(f"{oracleurl}/upload_file/{session}",
-                                               files = { 'file': open(input, 'rb') },
-                                               auth = HTTPBasicAuth(user, pass_)
-                                               )
-                                     elif r.text != "INVALID":
-                                         break
-                                             
-                                             lapsed += waitfor
-                                                 time.sleep(waitfor)
+        f"{oracleurl}",
+        auth = HTTPBasicAuth(user, pass_)
+    )
 
-print("Collecting result")
-    
-    
+    print(r.text)
+
+    # submit the file
+    r = requests.post(
+        f"{oracleurl}/upload_file/{session}",
+        files = { 'file': open(input, 'rb') },
+        auth = HTTPBasicAuth(user, pass_)
+    )
+
+    hsh = r.text
+    print(hsh)
+
+    lapsed = 0
+    waitfor = 5
+    while lapsed <= 900: # no more than 15 mins
+
+        r = requests.get(
+            f"{oracleurl}/get_result/{session}/{hsh}",
+            auth = HTTPBasicAuth(user, pass_)
+        )
+        if r.text == "REQUEUE":
+            requests.post(f"{oracleurl}/upload_file/{session}",
+                    files = { 'file': open(input, 'rb') },
+                    auth = HTTPBasicAuth(user, pass_)
+            )
+        elif r.text != "INVALID":
+            break
+
+        lapsed += waitfor
+        time.sleep(waitfor)
+
+    print("Collecting result")
+
+
     try:
         DATA = StringIO(r.text)
-        
+
+    df = pd.read_csv(DATA)
+    print(df)
+    try:
         df = pd.read_csv(DATA)
         print(df)
         print("Non detected", df['non_benign'].values)
-        
+
+        val = df['non_benign'].values[0]
+        engines = df['engines'].values[0]
+
+        if val == 0 and engines >= 58:
+            print("Not detected as mal")
+            exit(1)
+    except Exception as e:
+        print(e)
+        return check_simple(oracleurl, checkoracle, user, pass_, session, input)
+
         val = df['non_benign'].values[0]
         engines = df['engines'].values[0]
         # Saving the csv file
         df.to_csv("/result.csv")
-        
+
         if val == 0 and engines >= 52:
             print("Not detected as mal")
             exit(1)
@@ -76,31 +89,31 @@ print("Collecting result")
 def check_multiple(oracleurl, checkoracle, user, pass_, session,files):
     print(f"Processing {len(files)} files")
     global WHITELIST
-    
+
     # check count
     r = requests.get(
-                     f"{oracleurl}",
-                     auth = HTTPBasicAuth(user, pass_)
-                     )
-        
-                     print(r.text)
-                     
-                     
-                     # submit all the files first
-                     hashes = []
-                     for input in files:
-                         r = requests.post(
-                                           f"{oracleurl}/upload_file/{session}",
-                                           files = { 'file': open(input, 'rb') },
-                                           auth = HTTPBasicAuth(user, pass_)
-                                           )
-                             
-                             hsh = r.text
-                                 print(hsh)
-                                     hashes.append(dict(hsh=hsh, checked=False, f=input))
+        f"{oracleurl}",
+        auth = HTTPBasicAuth(user, pass_)
+    )
 
-# Then check hash by hash
-lapsed = 0
+    print(r.text)
+
+
+    # submit all the files first
+    hashes = []
+    for input in files:
+        r = requests.post(
+            f"{oracleurl}/upload_file/{session}",
+            files = { 'file': open(input, 'rb') },
+            auth = HTTPBasicAuth(user, pass_)
+        )
+
+        hsh = r.text
+        print(hsh)
+        hashes.append(dict(hsh=hsh, checked=False, f=input))
+
+    # Then check hash by hash
+    lapsed = 0
     waitfor = 1
     print("Collecting result")
     while True:
@@ -111,66 +124,68 @@ lapsed = 0
                 break
             if meta['checked']:
                 continue
-        
+
             hsh = meta['hsh']
             r = requests.get(
-                             f"{oracleurl}/get_result/{session}/{hsh}",
-                             auth = HTTPBasicAuth(user, pass_)
-                             )
-                
-                             if r.text == "REQUEUE":
-                                 input = meta['f']
-                                     requests.post(f"{oracleurl}/upload_file/{session}",
-                                                   files = { 'file': open(input, 'rb') },
-                                                   auth = HTTPBasicAuth(user, pass_)
-                                                   )
-                                         elif r.text != "INVALID":
-                                             continue
-                                                 
-                                                 DATA = StringIO(r.text)
-                                                     print(hsh)
-                                                         df = pd.read_csv(DATA)
-                                                             
-                                                             try:
-                                                                 print(df)
-                                                                     print("Non detected", df['non_benign'].values)
-                                                                         
-                                                                         val = df['non_benign'].values[0]
-                                                                             engines = df['engines'].values[0]
-                                                                                 
-                                                                                 if val == 0:
-                                                                                     print("Not detected as mal")
-                                                                                         exit(1)
-                                                                                             # Remove hsh from hashes
-                                                                                             meta['checked'] = True
-                                                                                                 except Exception as e:
-                                                                                                     print(e)
-                                                                                                         pass
-                                                                                                     
-                                                                                                     if complete:
-                                                                                                         break
-                                                                                                             
-                                                                                                             time.sleep(waitfor)
-                                                                                                                 lapsed += waitfor
-                                                                                                                     
-                                                                                                                     if lapsed >= 900:
-                                                                                                                         break
+                f"{oracleurl}/get_result/{session}/{hsh}",
+                auth = HTTPBasicAuth(user, pass_)
+            )
+
+            if r.text == "REQUEUE":
+                input = meta['f']
+                requests.post(f"{oracleurl}/upload_file/{session}",
+                        files = { 'file': open(input, 'rb') },
+                        auth = HTTPBasicAuth(user, pass_)
+                )
+            elif r.text != "INVALID":
+                continue
+
+            DATA = StringIO(r.text)
+            print(hsh)
+            df = pd.read_csv(DATA)
+
+            try:
+                print(df)
+                print("Non detected", df['non_benign'].values)
+
+                val = df['non_benign'].values[0]
+                engines = df['engines'].values[0]
+
+                if val == 0:
+                    print("Not detected as mal")
+                    exit(1)
+                # Remove hsh from hashes
+                meta['checked'] = True
+            except Exception as e:
+                print(e)
+                pass
+
+
+        if complete:
+            break
+
+        time.sleep(waitfor)
+        lapsed += waitfor
+
+        if lapsed >= 900:
+            break
+
 
 
 
 if __name__ == '__main__':
     oracleurl = sys.argv[1]
     checkoracle = sys.argv[2]
-    
+
+
     # auth to the service
     user = sys.argv[3]
     pass_ = sys.argv[4]
     session = sys.argv[5]
     input = sys.argv[6:] # all remainig files
-    
+
     if len(input) == 1:
         check_simple(oracleurl, checkoracle, user, pass_, session, input[0])
     else:
         check_multiple(oracleurl, checkoracle, user, pass_, session, input)
-
 
